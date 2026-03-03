fdc568ec3a87965756f1f96d7fec9284e264d146
MONU PAL <monupal@192.168.1.30>
2026-03-04 00:04:59 +0530

    hey

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,3 +1,17 @@
+// import express from "express";
+// import {login,register, updateprofile } from "../controllers/user.controllers.js";
+// import isAuthenticated from "../middlewares/isAuthenticated.js";
+
+
+
+// const router = express.Router();
+
+// router.route("/register").post(register);
+// router.route("/login").post(login);
+// router.route("/profile/update").post(isAuthenticated,updateprofile);
+// export default router;
+
+
 import express from "express";
 import {login,register, updateprofile } from "../controllers/user.controllers.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
@@ -9,4 +23,10 @@ const router = express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/profile/update").post(isAuthenticated,updateprofile);
+
+// use these things
+// router.post('/register', register)
+
+// router.post('/login', login)
+// router.post('/profile/update', isAuthenticated, updateprofile)
 export default router;
